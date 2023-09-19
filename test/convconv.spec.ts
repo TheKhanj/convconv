48a5168ef731e61d385c33431e0115499c8d493a
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-09-19 22:24:49 +0330

    improve tests

--- test/convconv.spec.ts
+++ test/convconv.spec.ts
@@ -9,40 +9,54 @@ function cartesian(...a: any[][]): any[] {
 }
 
 describe("convconv", () => {
-  const tests: { [key: string]: string } = {
-    kebab: "my-random-sentence-123",
-    snake: "my_random_sentence_123",
-    camel: "myRandomSentence123",
-    pascal: "MyRandomSentence123",
-    screamingKebab: "MY_RANDOM_SENTENCE_123",
-  };
+  const tests: { [key: string]: string }[] = [
+    {
+      kebab: "my-random-sentence-123",
+      snake: "my_random_sentence_123",
+      camel: "myRandomSentence123",
+      pascal: "MyRandomSentence123",
+      screamingKebab: "MY_RANDOM_SENTENCE_123",
+    },
+    {
+      kebab: "my-random-sentence-123-and-something-after",
+      snake: "my_random_sentence_123_and_something_after",
+      camel: "myRandomSentence123AndSomethingAfter",
+      pascal: "MyRandomSentence123AndSomethingAfter",
+      screamingKebab: "MY_RANDOM_SENTENCE_123_AND_SOMETHING_AFTER",
+    },
+  ];
 
-  const types = Object.keys(tests);
+  const types = Object.keys(tests[0]);
 
-  test.each(cartesian(types, types))(
-    "converting %s to %s",
-    (fromType: Types, toType: Types) => {
-      const from = tests[fromType];
-      const to = tests[toType];
+  tests.forEach((tests) =>
+    test.each(cartesian(types, types))(
+      "converting %s to %s",
+      (fromType: Types, toType: Types) => {
+        const from = tests[fromType];
+        const to = tests[toType];
 
-      expect(convconv.fromType(fromType, from).toType(toType)).toBe(to);
-    }
+        expect(convconv.fromType(fromType, from).toType(toType)).toBe(to);
+      }
+    )
   );
 
-  test.each(cartesian(types, types))(
-    "autoFrom should convert %s to %s",
-    (fromType: Types, toType: Types) => {
-      const from = tests[fromType];
-      const to = tests[toType];
+  tests.forEach((tests) =>
+    test.each(cartesian(types, types))(
+      "autoFrom should convert %s to %s",
+      (fromType: Types, toType: Types) => {
+        const from = tests[fromType];
+        const to = tests[toType];
 
-      expect(convconv.autoFrom(from).toType(toType)).toBe(to);
-    }
+        expect(convconv.autoFrom(from).toType(toType)).toBe(to);
+      }
+    )
   );
 
   test.each([
     "123NumberPrefix",
     "snake_then-kebab",
     "camelCaseButThenAnAtSign@",
+    "Random_sTring-with_nOConvention",
   ])(
     "getType should throw ConventionNotFoundError for invalid inputs",
     (from: string) => {
