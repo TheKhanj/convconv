fc390f0031a55969c4deff295e320e865c0fcae5
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-09-19 21:37:04 +0330

    add test for autoFrom

--- test/convconv.spec.ts
+++ test/convconv.spec.ts
@@ -8,7 +8,7 @@ function cartesian(...a: any[][]): any[] {
   return a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())));
 }
 
-describe("/utils/naming-convention", () => {
+describe("convconv", () => {
   const tests: { [key: string]: string } = {
     kebab: "my-random-sentence",
     snake: "my_random_sentence",
@@ -26,6 +26,16 @@ describe("/utils/naming-convention", () => {
       const to = tests[toType];
 
       expect(convconv.fromType(fromType, from).toType(toType)).toBe(to);
-    },
+    }
+  );
+
+  test.each(cartesian(types, types))(
+    "autoFrom should convert %s to %s",
+    (fromType: Types, toType: Types) => {
+      const from = tests[fromType];
+      const to = tests[toType];
+
+      expect(convconv.autoFrom(from).toType(toType)).toBe(to);
+    }
   );
 });
